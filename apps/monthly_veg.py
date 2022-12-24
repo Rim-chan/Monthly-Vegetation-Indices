3d363484f68c66dfed178f432feb18f44d7a0382
Rim Sleimi <64995617+Rim-chan@users.noreply.github.com>
2022-12-24 16:54:57 +0100

    Update monthly_veg.py

--- apps/monthly_veg.py
+++ apps/monthly_veg.py
@@ -4,6 +4,14 @@ import ee, geemap
 import time
 geemap.ee_initialize()
 
+from src.args import *
 from src.cloud_mask import *
 from src.indices import *
 from src.monthly_composites import *
+
+
+def app():
+    st.title("Monthly Vegetation Indices")
+    
+    start_time = time.time()
+    args = get_main_args()
