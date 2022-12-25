00b0a3dc8900611727087ff50ab7192e80741e9a
Rim Sleimi <64995617+Rim-chan@users.noreply.github.com>
2022-12-25 09:09:10 +0100

    Update monthly_veg_idx.py

--- apps/monthly_veg_idx.py
+++ apps/monthly_veg_idx.py
@@ -40,4 +40,42 @@ def app():
     
     col1, col2 = st.columns(2)
     country = col1.selectbox("Indices", input_indcies, label_visibility="collapsed")    
-    date = col2.selectbox("Dates", dates, label_visibility="collapsed")
+    d = col2.selectbox("Dates", dates, label_visibility="collapsed")
+    
+    @st.cache(allow_output_mutation=True)
+    def get_S2_dataset():
+        
+        S2Col = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED") \
+              .filterBounds(COUNTRY)\
+              .select(['QA60', 'B2', 'B3', 'B4', 'B8', 'B12'])\
+              .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', args.cloud_coverage_percentage))\
+              .map(cloud_mask)\
+              .map(compute_msavi2) \
+              .map(compute_savi) \
+              .map(compute_ndvi) \
+              .map(compute_ndwi) 
+
+        monthly_ndvi = monthly_Data (S2Col.select('NDVI'), args.years, list(args.calendar.values()))  
+        monthly_ndwi = monthly_Data (S2Col.select('NDWI'), args.years, list(args.calendar.values()))  
+        monthly_savi = monthly_Data (S2Col.select('SAVI'), args.years, list(args.calendar.values()))  
+        monthly_msavi2 = monthly_Data (S2Col.select('MSAVI2'), args.years, list(args.calendar.values())) 
+
+        ndvi_img_list = monthly_ndvi.toList(monthly_ndvi.size().getInfo())
+        ndwi_img_list = monthly_ndwi.toList(monthly_ndwi.size())
+        savi_img_list = monthly_savi.toList(monthly_savi.size())
+        msavi2_img_list = monthly_msavi2.toList(monthly_msavi2.size())
+
+        return ndvi_img_list, ndwi_img_list, savi_img_list, msavi2_img_list
+        
+        
+      
+    ndvi_img_list, ndwi_img_list, savi_img_list, msavi2_img_list =  get_S2_dataset()
+    
+    idx = tuple(dates).index(d.strftime("%B %Y"))
+    st.write(idx)
+    ndvi_image = ee.Image(ndvi_img_list.get(idx))
+    ndwi_image = ee.Image(ndwi_img_list.get(idx))
+    savi_image = ee.Image(savi_img_list.get(idx))
+    msavi2_image = ee.Image(msavi2_img_list.get(idx))
+    
+    
